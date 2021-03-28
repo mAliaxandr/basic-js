9b99a01e46a962dfe6bc095965c7b3ad108708fd
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-03-28 16:48:58 +0300

    feat: implement func createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+  
+  const arr = array;
+    if (!Array.isArray(arr)) {
+      return false;
+    }
+    if (array.length === 0) {
+      return null;
+    }
+            let resStr = '';
+            arr.forEach(i => {
+                if (typeof i === 'string') {
+                  let itenStr = i;
+                  while (itenStr.slice(0,1) == ' ') {
+                      itenStr = itenStr.slice(1, itenStr.length);
+                  }
+                  resStr = resStr + itenStr.slice(0,1).toUpperCase();
+              }   
+            })
+            function bubbleSort(arr) {
+                for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+                    for (var j = 0, endJ = endI - i; j < endJ; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                            var swap = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = swap;
+                        }
+                    }
+                }
+                return arr;
+            }
+            resArr = resStr.split('');
+            let sortedArr = bubbleSort(resArr);
+            const result = sortedArr.join('');
+            return result;
 };
