8487464dbe10ab33e7c3512c7f4eb81253dde13b
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-03-28 19:26:48 +0300

    feat: implement some func  transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  // throw new CustomError('Not implemented');
+  const array = arr;
+            let arrRes = [];
+            array.forEach((i, index) => {
+                if (i === '--double-next') {
+                    if (array[index + 1]) {
+                        arrRes.push(array[index + 1]);
+                    } 
+                } else if (i === '--double-prev') {
+                    if (array[index - 1]) {
+                        if (array[index - 1] != '_x_') {
+                            arrRes.push(array[index - 1]);
+                        } 
+                    }
+                } else if (i === '--discard-next') {
+                    array.splice(index+1,1,'_x_');
+                } else if (i === '--discard-prev') {
+                    if (arrRes[index -1] === array[index-1]) {
+                        arrRes.pop()
+                    }
+                    
+                } else {
+                    if (i != '_x_') {
+                        arrRes.push(i);
+                    }
+                }
+                
+                // console.log('i -- ', index);
+            }) 
+            return arrRes;
 };
